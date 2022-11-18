7bff06e718edf5ca0233d8975c8c4ac81048d67d
anasdeyra <anasdoura607@gmail.com>
2022-11-18 18:14:36 +0100

    update avatar

--- components/mblNavbar.jsx
+++ components/mblNavbar.jsx
@@ -5,12 +5,19 @@ import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/router";
 import UserId from "./userId";
 import { Metamask_comp_text, Metamask_comp_icon } from "./metamask/Metamask";
+import { Connection } from "@solana/web3.js";
 import {
   WalletMultiButton,
   WalletModalProvider,
 } from "@solana/wallet-adapter-react-ui";
 import SolanaWallet from "./SolanaWallet";
+import { useWallet } from "@solana/wallet-adapter-react";
 require("@solana/wallet-adapter-react-ui/styles.css");
+
+async function getbalance(publicKey) {
+  const connection = new Connection("https://api.devnet.solana.com");
+  return await connection.getBalance(publicKey);
+}
 
 const MblNavbar = ({ theme }) => {
   const { mblMenu } = useSelector((state) => state.counter);
@@ -19,6 +26,15 @@ const MblNavbar = ({ theme }) => {
   const router = useRouter();
   const [navItemValue, setNavItemValue] = useState(1);
   const [navText, setnavText] = useState("");
+  const { connected, wallet, publicKey } = useWallet();
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    if (!publicKey) return;
+    getbalance(publicKey).then((b) => {
+      setBalance(b);
+    });
+  }, [publicKey]);
 
   const handleItemDropdown = (e) => {
     const target = e.target.closest("li");
@@ -998,122 +1014,126 @@ const MblNavbar = ({ theme }) => {
           <SolanaWallet />
 
           {/* <!-- Profile --> */}
-          <div className="js-nav-dropdown group-dropdown relative">
-            {router.asPath === "/home/home_3" ? (
-              <button
-                className="dropdown-toggle border-jacarta-100 focus:bg-accent group hover:bg-accent ml-2 flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent border-transparent bg-white/[.15]"
-                onMouseEnter={() => setProfileShow(true)}
-                onMouseLeave={() => setProfileShow(false)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                  className=" h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white fill-white"
+          {connected && (
+            <div className="js-nav-dropdown group-dropdown relative">
+              {router.asPath === "/home/home_3" ? (
+                <button
+                  className="dropdown-toggle border-jacarta-100 focus:bg-accent group hover:bg-accent ml-2 flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent border-transparent bg-white/[.15]"
+                  onMouseEnter={() => setProfileShow(true)}
+                  onMouseLeave={() => setProfileShow(false)}
                 >
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"></path>
-                </svg>
-              </button>
-            ) : (
-              <button
-                className="dropdown-toggle border-jacarta-100 hover:bg-accent focus:bg-accent group dark:hover:bg-accent ml-2 flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent dark:border-transparent dark:bg-white/[.15]"
-                onMouseEnter={() => setProfileShow(true)}
-                onMouseLeave={() => setProfileShow(false)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                  className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
-                >
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"></path>
-                </svg>
-              </button>
-            )}
-
-            <div
-              className={
-                profileShow
-                  ? "dropdown-menu dark:bg-jacarta-800 group-dropdown-hover:opacity-100 group-dropdown-hover:visible !-right-4 !top-[85%] !left-auto z-10 min-w-[14rem] whitespace-nowrap rounded-xl bg-white transition-all will-change-transform before:absolute before:-top-3 before:h-3 before:w-full lg:absolute lg:grid lg:!translate-y-4 lg:py-4 lg:px-2 lg:shadow-2xl show lg:visible lg:opacity-100"
-                  : "dropdown-menu dark:bg-jacarta-800 group-dropdown-hover:opacity-100 group-dropdown-hover:visible !-right-4 !top-[85%] !left-auto z-10 min-w-[14rem] whitespace-nowrap rounded-xl bg-white transition-all will-change-transform before:absolute before:-top-3 before:h-3 before:w-full lg:absolute lg:grid lg:!translate-y-4 lg:py-4 lg:px-2 lg:shadow-2xl hidden lg:invisible lg:opacity-0"
-              }
-              onMouseEnter={() => setProfileShow(true)}
-              onMouseLeave={() => setProfileShow(false)}
-            >
-              <UserId
-                classes="js-copy-clipboard font-display text-jacarta-700 my-4 flex select-none items-center whitespace-nowrap px-5 leading-none dark:text-white"
-                userId="0x7a86c0b064171007716bbd6af96676935799a63e"
-                shortId={true}
-              />
-
-              <div className="dark:border-jacarta-600 border-jacarta-100 mx-5 mb-6 rounded-lg border p-4">
-                <span className="dark:text-jacarta-200 text-sm font-medium tracking-tight">
-                  Balance
-                </span>
-                <div className="flex items-center">
-                  <svg className="icon icon-ETH -ml-1 mr-1 h-[1.125rem] w-[1.125rem]">
-                    <use xlinkHref="/icons.svg#icon-ETH"></use>
-                  </svg>
-                  <span className="text-green text-lg font-bold">10 ETH</span>
-                </div>
-              </div>
-              <Link href="/user/avatar_6">
-                <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     width="24"
                     height="24"
-                    className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
+                    className=" h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white fill-white"
                   >
                     <path fill="none" d="M0 0h24v24H0z"></path>
                     <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"></path>
                   </svg>
-                  <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
-                    My Profile
-                  </span>
-                </a>
-              </Link>
-              <Link href="/profile/user_avatar">
-                <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
+                </button>
+              ) : (
+                <button
+                  className="dropdown-toggle border-jacarta-100 hover:bg-accent focus:bg-accent group dark:hover:bg-accent ml-2 flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent dark:border-transparent dark:bg-white/[.15]"
+                  onMouseEnter={() => setProfileShow(true)}
+                  onMouseLeave={() => setProfileShow(false)}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     width="24"
                     height="24"
-                    className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
+                    className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
                   >
                     <path fill="none" d="M0 0h24v24H0z"></path>
-                    <path d="M9.954 2.21a9.99 9.99 0 0 1 4.091-.002A3.993 3.993 0 0 0 16 5.07a3.993 3.993 0 0 0 3.457.261A9.99 9.99 0 0 1 21.5 8.876 3.993 3.993 0 0 0 20 12c0 1.264.586 2.391 1.502 3.124a10.043 10.043 0 0 1-2.046 3.543 3.993 3.993 0 0 0-3.456.261 3.993 3.993 0 0 0-1.954 2.86 9.99 9.99 0 0 1-4.091.004A3.993 3.993 0 0 0 8 18.927a3.993 3.993 0 0 0-3.457-.26A9.99 9.99 0 0 1 2.5 15.121 3.993 3.993 0 0 0 4 11.999a3.993 3.993 0 0 0-1.502-3.124 10.043 10.043 0 0 1 2.046-3.543A3.993 3.993 0 0 0 8 5.071a3.993 3.993 0 0 0 1.954-2.86zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
+                    <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"></path>
                   </svg>
-                  <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
-                    Edit Profile
+                </button>
+              )}
+
+              <div
+                className={
+                  profileShow
+                    ? "dropdown-menu dark:bg-jacarta-800 group-dropdown-hover:opacity-100 group-dropdown-hover:visible !-right-4 !top-[85%] !left-auto z-10 min-w-[14rem] whitespace-nowrap rounded-xl bg-white transition-all will-change-transform before:absolute before:-top-3 before:h-3 before:w-full lg:absolute lg:grid lg:!translate-y-4 lg:py-4 lg:px-2 lg:shadow-2xl show lg:visible lg:opacity-100"
+                    : "dropdown-menu dark:bg-jacarta-800 group-dropdown-hover:opacity-100 group-dropdown-hover:visible !-right-4 !top-[85%] !left-auto z-10 min-w-[14rem] whitespace-nowrap rounded-xl bg-white transition-all will-change-transform before:absolute before:-top-3 before:h-3 before:w-full lg:absolute lg:grid lg:!translate-y-4 lg:py-4 lg:px-2 lg:shadow-2xl hidden lg:invisible lg:opacity-0"
+                }
+                onMouseEnter={() => setProfileShow(true)}
+                onMouseLeave={() => setProfileShow(false)}
+              >
+                <UserId
+                  classes="js-copy-clipboard font-display text-jacarta-700 my-4 flex select-none items-center whitespace-nowrap px-5 leading-none dark:text-white"
+                  userId={String(publicKey)}
+                  shortId={true}
+                />
+
+                <div className="dark:border-jacarta-600 border-jacarta-100 mx-5 mb-6 rounded-lg border p-4">
+                  <span className="dark:text-jacarta-200 text-sm font-medium tracking-tight">
+                    Balance
                   </span>
-                </a>
-              </Link>
-              <Link href="/login">
-                <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
-                    className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z"></path>
-                    <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM7 11V8l-5 4 5 4v-3h8v-2H7z"></path>
-                  </svg>
-                  <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
-                    Sign out
-                  </span>
-                </a>
-              </Link>
+                  <div className="flex items-center">
+                    <svg className="icon icon-ETH -ml-1 mr-1 h-[1.125rem] w-[1.125rem]">
+                      <use xlinkHref="/icons.svg#icon-ETH"></use>
+                    </svg>
+                    <span className="text-green text-lg font-bold">
+                      {(balance / 1000000000).toPrecision(4)} SOL
+                    </span>
+                  </div>
+                </div>
+                <Link href="/user/avatar_6">
+                  <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                      className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"></path>
+                    </svg>
+                    <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
+                      My Profile
+                    </span>
+                  </a>
+                </Link>
+                <Link href="/profile/user_avatar">
+                  <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                      className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path d="M9.954 2.21a9.99 9.99 0 0 1 4.091-.002A3.993 3.993 0 0 0 16 5.07a3.993 3.993 0 0 0 3.457.261A9.99 9.99 0 0 1 21.5 8.876 3.993 3.993 0 0 0 20 12c0 1.264.586 2.391 1.502 3.124a10.043 10.043 0 0 1-2.046 3.543 3.993 3.993 0 0 0-3.456.261 3.993 3.993 0 0 0-1.954 2.86 9.99 9.99 0 0 1-4.091.004A3.993 3.993 0 0 0 8 18.927a3.993 3.993 0 0 0-3.457-.26A9.99 9.99 0 0 1 2.5 15.121 3.993 3.993 0 0 0 4 11.999a3.993 3.993 0 0 0-1.502-3.124 10.043 10.043 0 0 1 2.046-3.543A3.993 3.993 0 0 0 8 5.071a3.993 3.993 0 0 0 1.954-2.86zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
+                    </svg>
+                    <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
+                      Edit Profile
+                    </span>
+                  </a>
+                </Link>
+                <Link href="/login">
+                  <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                      className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM7 11V8l-5 4 5 4v-3h8v-2H7z"></path>
+                    </svg>
+                    <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
+                      Sign out
+                    </span>
+                  </a>
+                </Link>
+              </div>
             </div>
-          </div>
+          )}
 
           {/* <!-- Dark Mode --> */}
           <button
